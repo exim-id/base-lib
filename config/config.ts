d34ec4f5cfafff2c5a74e726982a3f6ad1353af9
Achmad Rifa'i <janoko.java22@yahoo.com>
2023-10-22 00:36:53 +0700

    slimming db config

--- config/config.ts
+++ config/config.ts
@@ -3,11 +3,8 @@ import { Mutex, mongodb } from "../deps.ts"
 export const dbTrans = async (exec = async (_: mongodb.Db) => { }) => {
     const mu = new Mutex()
     await mu.acquire()
-    const dbUrl = Deno.env.get('DB_URL') || 'mongodb://localhost:27017/exim'
     const dbName = Deno.env.get('DB_NAME') || 'exim'
-    const dbUser = Deno.env.get('DB_USER') || ''
-    const dbPass = Deno.env.get('DB_PASSWORD') || ''
-    const cli = new mongodb.MongoClient(dbUrl, { auth: { password: dbPass, username: dbUser }, family: null, hints: null, localAddress: null, localPort: null, lookup: null })
+    const cli = cliCreate()
     try {
         await cli.connect()
         const session = cli.startSession()
@@ -28,15 +25,19 @@ export const dbTrans = async (exec = async (_: mongodb.Db) => { }) => {
 }
 
 export const dbConnection = async (exec = async (_: mongodb.Db) => { }) => {
-    const dbUrl = Deno.env.get('DB_URL') || 'mongodb://localhost:27017/exim'
     const dbName = Deno.env.get('DB_NAME') || 'exim'
-    const dbUser = Deno.env.get('DB_USER') || ''
-    const dbPass = Deno.env.get('DB_PASSWORD') || ''
-    const cli = new mongodb.MongoClient(dbUrl, { auth: { password: dbPass, username: dbUser }, family: null, hints: null, localAddress: null, localPort: null, lookup: null })
+    const cli = cliCreate()
     try {
         await cli.connect()
         await exec(cli.db(dbName))
     } finally {
         await cli.close()
     }
+}
+
+const cliCreate = () => {
+    const dbUrl = Deno.env.get('DB_URL') || 'mongodb://localhost:27017/exim'
+    const dbUser = Deno.env.get('DB_USER') || ''
+    const dbPass = Deno.env.get('DB_PASSWORD') || ''
+    return new mongodb.MongoClient(dbUrl, { auth: { password: dbPass, username: dbUser }, family: null, hints: null, localAddress: null, localPort: null, lookup: null })
 }

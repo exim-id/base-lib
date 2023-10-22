7cd3e09a38ed914199604a52df59e9eb3c358389
Jefri Herdi Triyanto <jefri.triyanto@ist.id>
2023-10-22 11:56:37 +0700

    fix deps.ts

--- mod.ts
+++ mod.ts
@@ -4,7 +4,7 @@ export * from "./file_cache.ts";
 export * from "./src/db.ts";
 export * from "./src/env.ts";
 export * from "./src/helper.ts";
-export * as HttpResponse from "./src/httpResponse.ts";
+export { default as HttpResponse } from "./src/httpResponse.ts";
 export * from "./src/jwt.ts";
 export * from "./src/path.ts";
 export * as Swagger from "./src/swagger.ts";

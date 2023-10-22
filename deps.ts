9d5b544a5df62debaffca21db4f85392671b3c72
Jefri Herdi Triyanto <jefri.triyanto@ist.id>
2023-10-22 09:31:58 +0700

    fix deps.ts

--- deps.ts
+++ deps.ts
@@ -7,9 +7,10 @@ export {
 
 export * as mongodb from "npm:mongodb";
 
-export * as jwt from "npm:jsonwebtoken";
+// @deno-types="npm:@types/jsonwebtoken@9.0.2"
+export * as jwt from "npm:jsonwebtoken@9.0.0";
 
-export * as uuid from "npm:uuid";
+export * as uuid from "npm:uuid@9.0.0";
 
 export { z } from "npm:zod";
 
@@ -28,16 +29,20 @@ export * as crypto from "https://deno.land/std@0.177.0/node/crypto.ts";
 // @deno-types="npm:@types/supertest"
 export * as supertest from "npm:supertest";
 
-// @deno-types="npm:@types/cookie-parser"
-export * as cookieParser from "npm:cookie-parser";
+// @deno-types="npm:@types/cookie-parser@1.4.3"
+export * as cookieParser from "npm:cookie-parser@1.4.6";
 
-// @deno-types="npm:@types/cors"
-export * as cors from "npm:cors";
+// @deno-types="npm:@types/cors@2.8.13"
+export * as cors from "npm:cors@2.8.5";
 
-export * as helmet from "npm:helmet";
+// @deno-types="npm:@types/jsonwebtoken@9.0.0"
+export * as jsonwebtoken from "npm:jsonwebtoken@9.0.2";
 
 // @deno-types="npm:@types/morgan@1.9.4"
 export * as morgan from "npm:morgan@1.10.0";
+
+// @deno-types="npm:@types/node-cron@3.0.8"
+export * as nodeCron from "npm:node-cron@3.0.2";
 
 export * as swaggerAutogen from "npm:swagger-autogen@2.23.1";
 
@@ -46,3 +51,5 @@ export * as SwaggerUIBundle from "npm:swagger-ui-dist@5.7.2";
 export { Status } from "https://deno.land/x/opine@2.3.4/deps.ts";
 
 export * as base64 from "https://deno.land/std@0.203.0/encoding/base64.ts";
+
+export * as helmet from "npm:helmet@7.0.0";

9373979af567a6342af3eb3aac9528be67b0cb14
Achmad Rifa'i <janoko.java22@yahoo.com>
2023-10-24 20:07:14 +0700

    minio function

--- src/minio.ts
+++ src/minio.ts
@@ -1,7 +1,7 @@
 import { minio } from "../deps.ts";
 import { Minio } from "./env.ts";
 
-export const minioClient = ()=>new minio.Client({
+export const minioClient = () => new minio.Client({
   endPoint: Minio.MINIO_URL,
   accessKey: Minio.MINIO_ACCESS_KEY, 
   secretKey: Minio.MINIO_SECRET_KEY,   
